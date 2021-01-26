c1478e53637d40b6cde4519f212c1787eb40713a
lazarev <k.lazarev@cleack.com>
2021-01-26 15:26:18 +0200

    Add header and footer

---
+++ components/footer/app-footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer>
+    Footer
+  </footer>
+</template>
+
+<script>
+    export default {
+        name: "app-footer"
+    }
+</script>
+
+<style>
+footer {
+  height: 100px;
+  background: green;
+}
+</style>
---
+++ components/header/app-header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <div class="container">
+      header
+    </div>
+  </header>
+</template>
+
+<script>
+    export default {
+        name: "app-header",
+        data(){
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style>
+  header {
+    height: 100px;
+    background: red;
+  }
+</style>
