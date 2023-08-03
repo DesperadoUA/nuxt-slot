04bbf12c1e3aa31258179b308c1d60711b87d43c
lazarev <lazarev-konstant@mail.ru>
2023-08-03 10:56:22 +0300

    add headMixin for blog page

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,9 +1,9 @@
 <template>
   <main>
-      <app_banner></app_banner>
-      <app_h1 :value="data.body.h1"></app_h1>
-      <app_blog_card :posts="data.body.blog"></app_blog_card>
-      <app_content :value="data.body.content"></app_content>
+      <app_banner />
+      <app_h1 :value="data.body.h1" />
+      <app_blog_card :posts="data.body.blog" />
+      <app_content :value="data.body.content" />
   </main>
 </template>
 <script>
@@ -14,8 +14,10 @@
     import app_blog_card from '~/components/blog_card/app_blog_card'
     import config from '~/config'
     import helper from '~/helpers'
+    import headMixin from '~/mixins/head.js'
 export default {
     name: "blog",
+    mixins: [headMixin],
     data: () => {
         return {
             data: {}
@@ -34,151 +36,6 @@ export default {
         data.body.headerLinks = helper.hreflang(data.body.hreflang)
         store.dispatch('options/setHrefLang', data.body.headerLinks)
         return {data}
-    },
-    head() {
-        return {
-            title: this.data.body.meta_title,
-            meta: [
-                {
-                    hid: 'description',
-                    name: 'description',
-                    content: this.data.body.meta_description
-                },
-                {
-                    hid: 'classification',
-                    name: 'classification',
-                    content: this.data.body.meta.classification
-                },
-                {
-                    hid: 'distribution',
-                    name: 'distribution',
-                    content: this.data.body.meta.distribution
-                },
-                {
-                    hid: 'author',
-                    name: 'author',
-                    content: this.data.body.meta.author
-                },
-                {
-                    hid: 'creator',
-                    name: 'creator',
-                    content: this.data.body.meta.creator
-                },
-                {
-                    hid: 'copyright',
-                    name: 'copyright',
-                    content: this.data.body.meta.copyright
-                },
-                {
-                    hid: 'publisher',
-                    name: 'publisher',
-                    content: this.data.body.meta.publisher
-                },
-                {
-                    hid: 'geo.placename',
-                    name: 'geo.placename',
-                    content: this.data.body.meta.placename
-                },
-                {
-                    hid: 'geo.position',
-                    name: 'geo.position',
-                    content: this.data.body.meta.position
-                },
-                {
-                    hid: 'geo.region',
-                    name: 'geo.region',
-                    content: this.data.body.meta.region
-                },
-                {
-                    hid: 'ICBM',
-                    name: 'ICBM',
-                    content: this.data.body.meta.ICBM
-                },
-                 {
-                    hid: 'robots',
-                    name: 'robots',
-                    content: this.data.body.meta.robots
-                },
-                // og //
-                {
-                    hid: 'og:locale',
-                    property: 'og:locale',
-                    content: 'ru-UA'
-                },
-                {
-                    hid: 'og:type',
-                    property: 'og:type',
-                    content: 'website'
-                },
-                {
-                    hid: 'og:title',
-                    property: 'og:title',
-                    content: this.data.body.meta_title
-                },
-                {
-                    hid: 'og:description',
-                    property: 'og:description',
-                    content: this.data.body.meta_description
-                },
-                {
-                    hid: 'og:url',
-                    property: 'og:url',
-                    content: this.data.body.currentUrl,
-                },
-                {
-                    hid: 'og:article:section',
-                    property: 'og:article:section',
-                    content: this.data.body.h1,
-                },
-                {
-                    hid: 'og:article:published_time',
-                    property: 'og:article:published_time',
-                    content: this.data.body.date,
-                },
-                {
-                    hid: 'og:article:modified_time',
-                    property: 'og:article:modified_time',
-                    content: this.data.body.date_modified,
-                },
-                {
-                    hid: 'og:image',
-                    property: 'og:image',
-                    content: this.data.body.thumbnail,
-                },
-                // end og //
-                // twitter //
-                {
-                    hid: 'twitter:card',
-                    name: 'twitter:card',
-                    content: 'summary'
-                },
-                {
-                    hid: 'twitter:title',
-                    name: 'twitter:title',
-                    content: this.data.body.meta_title,
-                },
-                {
-                    hid: 'twitter:description',
-                    name: 'twitter:description',
-                    content: this.data.body.meta_description,
-                },
-                {
-                    hid: 'twitter:image',
-                    name: 'twitter:image',
-                    content: this.data.body.thumbnail,
-                },
-                {
-                    hid: 'twitter:url',
-                    name: 'twitter:url',
-                    content: this.data.body.currentUrl,
-                },
-                // end twitter //
-            ],
-             link: [
-                   { rel: 'canonical', href: this.data.body.currentUrl},
-                   ...this.data.body.headerLinks
-                ]
-        }
     }
 }
 </script>
