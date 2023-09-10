eaaa801bd714b391233d3347a677fba264c8fca4
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2023-09-10 16:35:01 +0300

    Add amp script for form review

--- components/form_review/app_form_review_amp.vue
+++ components/form_review/app_form_review_amp.vue
@@ -1,70 +1,78 @@
 <template>
-<amp-script layout="container" :src="`${config.BASE_URL[config.LANG]}/amp-review-form.js`" class="sample">
-  <section class="form_review">
-      <div class="container">
-          <div class="reviews_title">{{translates.LEAVE_YOUR_REVIEW[config.LANG]}}</div>
-          <div class="reviews_form_wrapper">
-              <div class="reviews_form_wrapper_input">
-                  <input type="text" 
-                         class="form_input jsInputName" 
-                         name="name"
-                         :placeholder="translates.NAME[config.LANG]"
-                         >
-                  <input type="text" 
-                         class="form_input jsInputEmail" 
-                         name="email"
-                         :placeholder="translates.OUR_EMAIL[config.LANG]"
-                         > 
-              </div>
-              <textarea class="form_textarea jsInputMsg" 
-                        name="msg"
-                        :placeholder="translates.WRITE_TEXT_YOUR_REVIEW[config.LANG]"
-              ></textarea>     
-              <div class="form_review_rating">
-                    <span class="jsRatingContainer">
-                        <amp-img v-for="item in 10" 
-                            :key="item" 
-                            :src="(item) <= currentRating ? '/img/Star.png': '/img/Star_empty.png'" 
-                            class="rating_stars jsRating"
-                            width="16" height="16"
-                            :data-index="item"
-                        ></amp-img>
-                    </span>
-                    <span class="form_rating_value"><b class="jsCurrentRating">{{currentRating}}</b>/10</span>
-              </div>   
-              <div class="form_review_submit">
-                  <button class="form_review_send"
-                    :data-apiUrl="config.API_URL[config.LANG]+'reviews/'" 
-                    :data-post-id="id"
-                    :data-translate-enter-name="translates.ENTER_YOUR_NAME[config.LANG]"
-                    :data-translate-invalid-postal-address="translates.INVALID_POSTAL_ADDRESS[config.LANG]"
-                    :data-translate-add-your-review="translates.ADD_YOUR_REVIEW[config.LANG]"
-                    :data-translate-review-added-for-moderation="translates.REVIEW_ADDED_FOR_MODERATION[config.LANG]"
-                  >{{translates.SEND[config.LANG]}}</button>
-              </div>
-              <div class="error jsErrorContainer"></div>            
-          </div>
-      </div>
-  </section>
-</amp-script>
+	<amp-script layout="container" :src="ReviewFormPathScript" class="sample">
+		<section class="form_review">
+			<div class="container">
+				<div class="reviews_title">{{ translates.LEAVE_YOUR_REVIEW[config.LANG] }}</div>
+				<div class="reviews_form_wrapper">
+					<div class="reviews_form_wrapper_input">
+						<input type="text" class="form_input jsInputName" name="name" :placeholder="translates.NAME[config.LANG]" />
+						<input
+							type="text"
+							class="form_input jsInputEmail"
+							name="email"
+							:placeholder="translates.OUR_EMAIL[config.LANG]"
+						/>
+					</div>
+					<textarea
+						class="form_textarea jsInputMsg"
+						name="msg"
+						:placeholder="translates.WRITE_TEXT_YOUR_REVIEW[config.LANG]"
+					></textarea>
+					<div class="form_review_rating">
+						<span class="jsRatingContainer">
+							<amp-img
+								v-for="item in 10"
+								:key="item"
+								:src="item <= currentRating ? '/img/Star.png' : '/img/Star_empty.png'"
+								class="rating_stars jsRating"
+								width="16"
+								height="16"
+								:data-index="item"
+							></amp-img>
+						</span>
+						<span class="form_rating_value"
+							><b class="jsCurrentRating">{{ currentRating }}</b
+							>/10</span
+						>
+					</div>
+					<div class="form_review_submit">
+						<button
+							class="form_review_send"
+							:data-apiUrl="config.API_URL[config.LANG] + 'reviews/'"
+							:data-post-id="id"
+							:data-translate-enter-name="translates.ENTER_YOUR_NAME[config.LANG]"
+							:data-translate-invalid-postal-address="translates.INVALID_POSTAL_ADDRESS[config.LANG]"
+							:data-translate-add-your-review="translates.ADD_YOUR_REVIEW[config.LANG]"
+							:data-translate-review-added-for-moderation="translates.REVIEW_ADDED_FOR_MODERATION[config.LANG]"
+						>
+							{{ translates.SEND[config.LANG] }}
+						</button>
+					</div>
+					<div class="error jsErrorContainer"></div>
+				</div>
+			</div>
+		</section>
+	</amp-script>
 </template>
 
 <script>
-    import translateMixin from '~/mixins/translate.js'
-    export default {
-        name: "app_form_review",
-        props: ['id'],
-        mixins: [translateMixin],
-        data(){
-            return {
-               name: '',
-               email: '',
-               text: '',
-               msg: [],
-               currentRating: 1
-            }
-        },
-        /*
+import translateMixin from '~/mixins/translate.js'
+import { REVIEW_FORM as ReviewFormPathScript } from '~/config/ampPathScript'
+export default {
+	name: 'app_form_review',
+	props: ['id'],
+	mixins: [translateMixin],
+	data() {
+		return {
+			name: '',
+			email: '',
+			text: '',
+			msg: [],
+			currentRating: 1,
+			ReviewFormPathScript
+		}
+	}
+	/*
         methods: {
             chengRating(item){
                 this.currentRating = item
@@ -92,5 +100,5 @@
               }
             }
         }*/
-    }
+}
 </script>
